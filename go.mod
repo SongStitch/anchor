16cf3e5c67912ca083955266e26ea5248ed60dc4
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2024-03-25 21:35:10 -0400

    feat: add basic code to parse the template dockerfile and add the digest to base images

--- go.mod
+++ go.mod
@@ -1,3 +1,29 @@
 module github.com/SongStitch/docker-lock
 
 go 1.22.1
+
+require (
+	github.com/containerd/stargz-snapshotter/estargz v0.15.1 // indirect
+	github.com/containerd/typeurl/v2 v2.1.1 // indirect
+	github.com/docker/cli v25.0.3+incompatible // indirect
+	github.com/docker/distribution v2.8.2+incompatible // indirect
+	github.com/docker/docker v25.0.3+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.8.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-containerregistry v0.19.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/moby/buildkit v0.13.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/vbatts/tar-split v0.11.5 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/google/go-containerregistry/pkg/crane"
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	content, err := os.Open("Dockerfile.template")
+	if err != nil {
+		panic(err)
+	}
+
+	defer content.Close()
+	result, err := parser.Parse(content)
+	if err != nil {
+		panic(err)
+	}
+
+	node := result.AST
+	printNode(node)
+
+	parseNode(node)
+
+	var builder strings.Builder
+	writeDockerfile(&builder, node)
+	os.WriteFile("Dockerfile", []byte(builder.String()), 0644)
+}
+
+func writeDockerfile(builder *strings.Builder, node *parser.Node) {
+	builder.WriteString(node.Value)
+	for _, child := range node.Children {
+		writeDockerfile(builder, child)
+		builder.WriteString("\n")
+	}
+
+	if node.Next != nil {
+		builder.WriteString(" ")
+		writeDockerfile(builder, node.Next)
+	}
+}
+
+func printNode(node *parser.Node) {
+	fmt.Println(node.Value)
+	for _, child := range node.Children {
+		printNode(child)
+	}
+
+	if node.Next != nil {
+		printNode(node.Next)
+	}
+}
+
+func parseNode(node *parser.Node) {
+	if node == nil {
+		return
+	}
+
+	if node.Value == "FROM" {
+		attachDockerSha(node.Next)
+	} else if node.Next != nil {
+		parseNode(node.Next)
+	}
+
+	for _, child := range node.Children {
+		parseNode(child)
+	}
+}
+
+func attachDockerSha(node *parser.Node) {
+	if node == nil {
+		return
+	}
+	digest, err := crane.Digest(node.Value)
+	if err != nil {
+		panic(err)
+	}
+	node.Value = fmt.Sprintf("%s@%s", node.Value, digest)
 }
