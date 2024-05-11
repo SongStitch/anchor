7597530ed7165f09391576c385cb62762b7044ae
TheDen <deniskhoshaba@gmail.com>
2024-05-11 19:45:50 +1000

    cleanup

--- images.go
+++ images.go
@@ -26,3 +26,23 @@ func attachDockerSha(node *parser.Node) (string, error) {
 	node.Value = fmt.Sprintf("%s@%s", node.Value, digest)
 	return node.Value, nil
 }
+
+func writeDockerfile(builder *strings.Builder, node *parser.Node, useOriginal bool) {
+	if node.Value == "FROM" || node.Value == "RUN" {
+		useOriginal = false
+	}
+	if useOriginal {
+		builder.WriteString(node.Original)
+	} else {
+		builder.WriteString(node.Value)
+	}
+	for _, child := range node.Children {
+		writeDockerfile(builder, child, useOriginal)
+		builder.WriteString("\n")
+	}
+
+	if node.Next != nil {
+		builder.WriteString(" ")
+		writeDockerfile(builder, node.Next, useOriginal)
+	}
+}
